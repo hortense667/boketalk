c2ad957727bcea2ad4e2006395f0b2f6763e7636
hortense667 <satoshi.endo@gmail.com>
2023-07-08 01:43:53 +0900

    cmt 16

--- boketalk.py
+++ boketalk.py
@@ -1,30 +1,35 @@
 import streamlit as st
 import openai
+from langdetect import detect
 
 # API key is obtained from the Streamlit secrets
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-def translate_to_japanese(text):
+def detect_language(text):
+    return detect(text)
+
+def translate(text, target_language):
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt=f"Translate the following English text to Japanese:\n{text}",
+        prompt=f"Translate the following {'English' if target_language == 'ja' else 'Japanese'} text to {'Japanese' if target_language == 'ja' else 'English'}:\n{text}",
         max_tokens=60
     )
     return response.choices[0].text.strip()
 
 def generate_joke(text):
     response = openai.Completion.create(
-        engine="GPT-4",
+        engine="text-davinci-002",
         prompt=f"Create a ironical joke based on this text:\n{text}",
         max_tokens=60
     )
     return response.choices[0].text.strip()
 
-st.title("TOKYO Boke Talk／東京ボケトーク")
-text = st.text_input("Enter some text (in English)")
+st.title("TOKYO Boke Talk ／ 東京ボケトーク")
+text = st.text_input("Enter some text")
 if st.button("Translate and Generate Joke"):
     if text:
-        translation = translate_to_japanese(text)
+        target_language = 'ja' if detect_language(text) == 'en' else 'en'
+        translation = translate(text, target_language)
         joke = generate_joke(text)
         st.write("Translation:", translation)
         st.write("Joke:", joke)
