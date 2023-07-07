ce21744f6c3ce19d79b7383a70441d61f180b2e5
hortense667 <satoshi.endo@gmail.com>
2023-07-08 01:53:13 +0900

    cmt 22

--- boketalk.py
+++ boketalk.py
@@ -10,8 +10,8 @@ def detect_language(text):
 
 def translate(text, target_language):
     response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Translate the following {'English' if target_language == 'ja' else 'Japanese'} text to {'Japanese／日本語でお願いします' if target_language == 'ja' else 'English'}:\n{text}",
+        engine="GPT-4",
+        prompt=f"Translate the following {'English' if target_language == 'ja' else 'Japanese'} text to {'Japanese／日本語でお願いします。' if target_language == 'ja' else 'English'}:\n{text}",
         max_tokens=60
     )
     return response.choices[0].text.strip()
