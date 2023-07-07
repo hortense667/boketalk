c834894807bed11c7769f1a7613eb431871e6bf4
hortense667 <satoshi.endo@gmail.com>
2023-07-08 01:20:46 +0900

    cmt 4

--- boketalk.py
+++ boketalk.py
@@ -1,21 +1,24 @@
 import streamlit as st
-from openai import OpenAI, models
-from openai.api_resources.completion import Completion
+import openai
 
 # API key is obtained from the Streamlit secrets
-openai = OpenAI(st.secrets["OPENAI_API_KEY"])
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 def translate_to_japanese(text):
-    model = models.Text2Text("text-davinci-002")
-    prompt = f"Translate the following English text to Japanese:\n{text}"
-    completion = Completion.create(model=model, prompt=prompt, max_tokens=60)
-    return completion.choices[0].text.strip()
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Translate the following English text to Japanese:\n{text}",
+        max_tokens=60
+    )
+    return response.choices[0].text.strip()
 
 def generate_joke(text):
-    model = models.Text2Text("text-davinci-002")
-    prompt = f"Create a joke based on this text:\n{text}"
-    completion = Completion.create(model=model, prompt=prompt, max_tokens=60)
-    return completion.choices[0].text.strip()
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Create a joke based on this text:\n{text}",
+        max_tokens=60
+    )
+    return response.choices[0].text.strip()
 
 st.title("OpenAI Translation and Joke Generator")
 text = st.text_input("Enter some text (in English)")
