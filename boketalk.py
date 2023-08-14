72de2a0183bf009f2b0427bbbeca1395227a9236
hortense667 <satoshi.endo@gmail.com>
2023-08-15 04:11:13 +0900

    cmt 11

--- boketalk.py
+++ boketalk.py
@@ -19,7 +19,7 @@ def translate(text, target_language):
     return response.choices[0].message["content"]
 
 def generate_joke(text): 
-    prompt =f"Create a silly joke based on this text:\n{text}"
+    prompt =f"Create a joke based on this text:\n{text}"
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -33,7 +33,7 @@ st.title("TOKYO Boke Talk／東京ボケトーク")
 
 st.markdown(
 """
--- ver.0.25 -- 2023/08/15 [@hortense667](https://twitter.com/hortense667)　 
+-- ver.0.26 -- 2023/08/15 [@hortense667](https://twitter.com/hortense667)　 
 """
 )
 
