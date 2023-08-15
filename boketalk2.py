1cf248625d9a1b8e3ca57a065bd07d388654af81
hortense667 <satoshi.endo@gmail.com>
2023-08-15 12:23:13 +0900

    cmt 01

--- boketalk2.py
+++ boketalk2.py
@@ -8,29 +8,76 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 def detect_language(text):
     return detect(text)
 
-def translate(text, target_language):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Translate the following {'English' if target_language == 'ja' else 'Japanese'} text to {'Japanese／日本語でお願いします。' if target_language == 'ja' else 'English'}:\n{text}",
-        max_tokens=60
+def translate(text, target_language): 
+    prompt =f"Translate the following {'English' if target_language == 'ja' else 'Japanese'} text to {'Japanese／日本語でお願いします。' if target_language == 'ja' else 'English'}:\n{text}"
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
     )
-    return response.choices[0].text.strip()
+    return response.choices[0].message["content"]
 
-def generate_joke(text):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Create a funny joke based on this text:\n{text}",
-        max_tokens=60
+def generate_joke(text): 
+    prompt =f"Create a puzzle-like joke based on this text:\n{text}"
+    messages = [{"role": "user", "content": prompt}]
+#    
+
+#    
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=messages,
+        temperature=0.7, # this is the degree of randomness of the model's output
     )
-    return response.choices[0].text.strip()
+    return response.choices[0].message["content"]
+
 
 st.title("TOKYO Boke Talk／東京ボケトーク")
 
 st.markdown(
 """
--- ver.0.1 -- 2023/07/08 [@hortense667](https://twitter.com/hortense667)　 
+-- ver.0.291 -- 2023/08/15 [@hortense667](https://twitter.com/hortense667)　 
 """
 )
+
+#---------------------------------------------------------------------
+#def init_messages():
+#    clear_button = st.sidebar.button("Clear Conversation", key="clear")
+#    if clear_button or "messages" not in st.session_state:
+#        st.session_state.messages = [
+#            SystemMessage(content="You are a helpful assistant.")
+#        ]
+#        st.session_state.costs = []
+
+def select_model():
+    model = st.sidebar.radio("Language model:", ("GPT-3.5-turbo", "GPT-4"))
+    if model == "GPT-3.5-turbo":
+        model_name = "gpt-3.5-turbo"
+    else:
+        model_name = "gpt-4"
+
+    # サイドバーにスライダーを追加し、temperatureを0から2までの範囲で選択可能にする
+    # 初期値は0.0、刻み幅は0.1とする
+    temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=0.0, step=0.01)
+
+    return ChatOpenAI(temperature=temperature, model_name=model_name)
+
+# サイドバーのタイトルを表示
+st.sidebar.title("Options")
+
+# Streamlitはmarkdownを書けばいい感じにHTMLで表示してくれます
+# (もちろんメイン画面でも使えます)
+#st.sidebar.markdown("## Costs")
+#st.sidebar.markdown("**Total cost**")
+#for i in range(3):
+#    st.sidebar.markdown(f"- ${i+0.01}")  # 説明のためのダミー
+
+#サイドバーからモデルを選ぶ
+llm = select_model()
+#コストはいま使わないので不要
+#init_messages()
+
+#---------------------------------------------------------------------
 
 text = st.text_input("Enter some text")
 if st.button("Translate and Generate Joke"):
